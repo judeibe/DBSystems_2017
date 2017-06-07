1d221c1667c5ff128b58d90458c4f197f3efc1c1
Jude Ibe (Toochukwu) <judeibe@gmail.com>
2017-06-07 01:34:41 -0500

    init commit
    Signed-off-by: Jude Ibe (Toochukwu) <judeibe@gmail.com>

---
+++ src/main/java/edu/govst/dbms/GroupProjectApplication.java
@@ -0,0 +1,12 @@
+package edu.govst.dbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/govst/dbms/GroupProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.govst.dbms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
