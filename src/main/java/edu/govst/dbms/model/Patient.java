52004eb3d4f4e643b24f930665da1c311eea1336
Jude Ibe (Toochukwu) <judeibe@gmail.com>
2017-06-15 00:56:00 -0500

    cleaned up models
    Signed-off-by: Jude Ibe (Toochukwu) <judeibe@gmail.com>

--- src/main/java/edu/govst/dbms/model/Patient.java
+++ src/main/java/edu/govst/dbms/model/Patient.java
@@ -1,25 +1,57 @@
 package edu.govst.dbms.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 
 @Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor()
 public class Patient {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private long patientId;
+
     private String firstName;
-    private String middleName;
+
     private String lastName;
+
+    private String gender;
+
     private LocalDate birthDate;
+
+    private long height;
+
+    private long weight;
+
+    private String nextOfKin;
+
+    private String phone;
+
+    private Boolean admitted;
+
+    private String otherDetails;
+
+    @ManyToOne
+    private Address address;
+
+    public Patient() {
+    }
+
+    public Patient(String firstName, String lastName, String gender, LocalDate birthDate, long height, long weight, String nextOfKin, String phone, Boolean admitted, String otherDetails, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.height = height;
+        this.weight = weight;
+        this.nextOfKin = nextOfKin;
+        this.phone = phone;
+        this.admitted = admitted;
+        this.otherDetails = otherDetails;
+        this.address = address;
+    }
 }
